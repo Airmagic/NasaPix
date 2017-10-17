b888fe6d6ec4975721e9fe916c0ab69431cdb9ea
Airmagic <babaphat@yahoo.com>
2017-10-17 14:38:43 -0500

    updating files
    bad descriptions :-p

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/fetch_picture', function(req, res, next){
+	if (req.query.random){
+		res.render('index', {title : 'todo: get random picture' });
+	}else{
+		res.render('index', {title : 'todo : picture'});
+	}
+})
+
 module.exports = router;
